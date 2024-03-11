b71c4083c6f75b0ddfed5e20fa325f94787928ba
Michael Keaveny <michaelkeaveny0@gmail.com>
2024-03-11 22:18:44 +0000

    Added swagger configuration to the DAL project

--- DeskBooker.DataAccess/Program.cs
+++ DeskBooker.DataAccess/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Data;
+﻿using Microsoft.OpenApi.Models;
+using System.Data;
 using System.Data.SqlClient;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,7 +7,18 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DeskBookerDBConnection");
 
 builder.Services.AddScoped<IDbConnection>(db => new SqlConnection(connectionString));
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1",
+        new OpenApiInfo
+        {
+            Title = "DeskBookerDAL",
+            Version = "v1"
+        });
+});
 
 var app = builder.Build();
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.Run();
