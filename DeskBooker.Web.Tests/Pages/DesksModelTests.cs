b060000f838b8a46e20fdc48013a6e586928c275
Michael Keaveny <michaelkeaveny0@gmail.com>
2024-03-14 21:12:06 +0000

    Added a test for the Desks page

---
+++ DeskBooker.Web.Tests/Pages/DesksModelTests.cs
@@ -0,0 +1,33 @@
+﻿using DeskBooker.Core.DataInterface;
+using DeskBooker.Core.Domain;
+using DeskBooker.Web.Integrations;
+using DeskBooker.Web.Pages;
+using Moq;
+
+namespace DeskBooker.Web.Tests.Pages
+{
+    public class DesksModelTests
+    {
+        [Fact]
+        public async Task ShouldGetAllDesks()
+        {
+            // Arrange
+            List<Desk> desks = new List<Desk>();
+            desks.Add(new Desk());
+            desks.Add(new Desk());
+            desks.Add(new Desk());
+
+            Mock<IDeskBookerDataAccessApiClient> apiClientMock = new Mock<IDeskBookerDataAccessApiClient>();
+            apiClientMock.Setup(x => x.GetAllDesks())
+                .ReturnsAsync(desks);
+
+            DesksModel desksModel = new DesksModel(apiClientMock.Object);
+
+            // Act
+            await desksModel.OnGet();
+
+            // Assert
+            Assert.Equal(desks, desksModel.Desks);
+        }
+    }
+}
